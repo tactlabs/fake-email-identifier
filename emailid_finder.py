370835087d1b35af9b26a541c6ca82715e4b2694
muthu mani <muthumani1999994@gmail.com>
2018-11-30 18:05:01 +0530

    python package

---
+++ emailid_finder.py
@@ -0,0 +1,30 @@
+import regex
+test_case ='''
+   	@yahoo.com 
+   	@hotmail.com 	
+ 	@aol.com 	
+ 	@gmail.com 	
+ 	@msn.com 	
+ 	@comcast.net 	
+ 	@hotmail.co.uk 	
+ 	@sbcglobal.net 	
+ 	@yahoo.co.uk 	
+ 	@yahoo.co.in 	
+ 	@bellsouth.net 	
+ 	@verizon.net 	
+ 	@earthlink.net 	
+ 	@cox.net 	
+ 	@rediffmail.com 	
+ 	@yahoo.ca 	
+ 	@btinternet.com 	
+ 	@charter.net 	
+ 	@shaw.ca 	
+    @ntlworld.com 	
+  	'''
+def check(mail_id):
+	after=regex.compile(mail_id[mail_id.index('@'):])
+	matches=after.findall(test_case)
+	if matches!=[]:
+		print("Domain exists....your email-id is real")
+	else:
+		print("domain doesnot exist..This is fake")
