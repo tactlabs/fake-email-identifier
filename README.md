31b46b937dc914ccd936f841b8b8e1ff8055a34f
mmblack <mmblack@mmblack-Lenovo-ideapad-320-15IKB>
2018-12-01 16:09:19 +0530

    0.3v

--- README.md
+++ README.md
@@ -6,15 +6,19 @@
 Run the following to install:
 
 '''python
-pip install emailid_finder
+
+pip install fake-email-identifier
 '''
+
 ##Usage
 
 '''python
 import emailid_finder as ef
 
 #Generate "True"
+
 ef.check('namubol@veanlo.com') //This id form https://temp-mail.org/en/
 
 #Generate "False"
+
 ef.check('<some real email>')
--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ with open("README.md","r")as fh:
     long_description = fh.read()
 
 setup(
-    name="emailid_finder",
-    version="0.0.1",
+    name="fake-email-identifier",
+    version="0.0.3",
     description="This will find the email-id is 'True' or 'Flase'",
     long_description=long_description,
     url="https://github.com/teamtact/fake-email-identifier",
     long_description_content_type="text/markdown",
-    py_modules=["emailid_finder"],
+    py_modules=["fake-email-identifier"],
     package_dir={'':'src'},
     classifiers=[
         "Programming Language :: Python :: 3",
